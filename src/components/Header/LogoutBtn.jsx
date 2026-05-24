a39012325ef94d5a02752e4916c8f12f53bf8b4d
MahilJain <Mahiljee001@gmail.com>
2026-05-24 17:57:14 +0530

    refactor: clean up code and improve component structure

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -3,7 +3,7 @@ import {useDispatch} from 'react-redux'
 import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
 
-function LogoutBtn({width = '100px'}) {
+function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
         authService.logout().then(() => {
